c637c8c9519d9b4ff97552e1460d6824efac7ed6
bmarques1995 <testebash@outlook.com>
2025-09-30 19:52:44 -0300

    Added light and dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,16 @@
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import './globals.css';
+import { useEffect } from "react";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  useEffect(() => {
+    document.documentElement.classList.add("dark");
+  }, []);
   return (
     <html lang="en">
       <body>
