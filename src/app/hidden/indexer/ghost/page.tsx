a3b4d0af8e61c4c9e87700a0b945254f4401fec5
bmarques1995 <testebash@outlook.com>
2025-10-07 20:01:40 -0300

    Added ghost page

---
+++ src/app/hidden/indexer/ghost/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function GhostImages() {
+    return (
+        <div>
+            <img src="/posts/Single_Source_Build.png" alt="Ghost image" />
+        </div>
+    );
+}
